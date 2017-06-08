cb5aa385506f5df6d936c7fc872d1aa9c3c76335
Bharat Tiwari <kkaphm@live.com>
2017-06-07 23:43:16 -0500

    app-header component added

---
+++ src/common/components/app-header/app-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './app-header.html',
+})
+
+export class AppHeaderComponent {
+}
--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from '../../common/components/app-header/app-header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent
   ],
   imports: [
     BrowserModule,
