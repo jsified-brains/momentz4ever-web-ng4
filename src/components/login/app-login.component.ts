0d7a4a224882607cc196b395c9a9d90dcb999e63
Graphicaction <rksharma2679@gmail.com>
2017-07-20 21:41:31 -0400

    Albums list

--- src/components/login/app-login.component.ts
+++ src/components/login/app-login.component.ts
@@ -11,16 +11,16 @@ export class LoginComponent {
 
     public onLoginClick(){
         //
-
-        FB.getLoginStatus((response) => {
-            if (response.status === 'connected') {
-                this.router.navigate(['./home']);
-            }
-            else {
-                FB.login((loginResponse)=>{
-                    this.router.navigate(['./home']);
-                });
-            }
-        });
+        this.router.navigate(['./home']);
+        // FB.getLoginStatus((response) => {
+        //     if (response.status === 'connected') {
+        //         this.router.navigate(['./home']);
+        //     }
+        //     else {
+        //         FB.login((loginResponse)=>{
+        //             this.router.navigate(['./home']);
+        //         });
+        //     }
+        // });
     }
 }
