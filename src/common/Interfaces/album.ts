e53ee2b4ed5c51b253d6690c91aa9a4b5d23b50d
Graphicaction <rksharma2679@gmail.com>
2017-08-07 21:45:38 -0400

    Added Photo Component, Album views

--- src/common/Interfaces/album.ts
+++ src/common/Interfaces/album.ts
@@ -1,5 +1,5 @@
 export interface Album {
-    id: string,
+    albumId: string,
     title:string,
     description:string,
     imageUrl:string,
---
+++ src/components/photo/app-photo.component.ts
@@ -0,0 +1,61 @@
+import { Component , OnInit,  ChangeDetectorRef } from '@angular/core';
+import { Router,ActivatedRoute,ParamMap } from '@angular/router';
+import { Photo } from '../../common/Interfaces/photo';
+import { PhotoService } from '../../common/services/photos.service';
+
+@Component({
+    selector: 'app-photo',
+    templateUrl: './app-photo.component.html'
+})
+
+export class PhotoComponent{
+    private id: string;
+    photos: Photo[] =[];
+
+    constructor(private route: ActivatedRoute,private router: Router,private photoService: PhotoService, private ref: ChangeDetectorRef){
+    }
+
+    getPhotos(){
+        
+         this.photoService
+        .getPhotos(this.id)
+        .subscribe(responseData => {
+            this.photos = responseData;
+            this.ref.detectChanges();
+        },
+        error => console.log(error));
+    }
+
+    public currentView='thumbnailView';
+
+    public setView(view:string){  
+        if (view === 'thumbnailView') 
+            this.currentView = 'thumbnailView';
+        else{
+        if (view === 'cardView') 
+            this.currentView = 'cardView';
+        }
+    }
+
+    onCommentClick(photo){
+        photo.commentArea=true;
+    }
+
+    private handleKeyDown(event: any,photo)
+    {
+        if (event.keyCode == 13)
+        {
+            photo.comment++;
+            photo.commentArea=false;
+        }
+    }
+    onLikeClick(photo){
+        photo.likeCount++;
+    }
+
+    ngOnInit(){
+        
+        this.id= this.route.snapshot.paramMap.get('albumId');
+        this.getPhotos();
+    }
+}
