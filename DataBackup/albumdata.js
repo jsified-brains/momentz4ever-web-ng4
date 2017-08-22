3f1f1f493e81dd30c5f5b1af3b5cdf68be888638
Graphicaction <rksharma2679@gmail.com>
2017-08-21 22:17:01 -0400

    DataBackup

---
+++ DataBackup/albumdata.js
@@ -0,0 +1,47 @@
+/* 1 */
+{
+    "_id" : ObjectId("5996356d1b46c304ecd23307"),
+    "ownerId" : "5990da68a633241198b377ee",
+    "title" : "Album1",
+    "description" : "This is the first album",
+    "coverImageUrl" : "http://drawabox.com/images/lesson3/ref_hibiscusflower.jpg",
+    "lastViewedOn" : ISODate("2017-05-08T04:00:00.000Z"),
+    "createdOn" : ISODate("2017-05-07T04:00:00.000Z"),
+    "__v" : 0
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("5996359e1b46c304ecd23308"),
+    "ownerId" : "5990da68a633241198b377ee",
+    "title" : "Album2",
+    "description" : "This is the second album",
+    "coverImageUrl" : "http://drawabox.com/images/lesson3/ref_hibiscusflower.jpg",
+    "lastViewedOn" : ISODate("2017-07-08T04:00:00.000Z"),
+    "createdOn" : ISODate("2017-05-06T04:00:00.000Z"),
+    "__v" : 0
+}
+
+/* 3 */
+{
+    "_id" : ObjectId("599635c31b46c304ecd23309"),
+    "ownerId" : "5990da68a633241198b377ee",
+    "title" : "Album3",
+    "description" : "This is the third album",
+    "coverImageUrl" : "http://drawabox.com/images/lesson3/ref_hibiscusflower.jpg",
+    "lastViewedOn" : ISODate("2017-08-08T04:00:00.000Z"),
+    "createdOn" : ISODate("2017-04-06T04:00:00.000Z"),
+    "__v" : 0
+}
+
+/* 4 */
+{
+    "_id" : ObjectId("599635ec1b46c304ecd2330a"),
+    "ownerId" : "5990da68a633241198b377ee",
+    "title" : "Album4",
+    "description" : "This is the forth album",
+    "coverImageUrl" : "http://drawabox.com/images/lesson3/ref_hibiscusflower.jpg",
+    "lastViewedOn" : ISODate("2017-08-07T04:00:00.000Z"),
+    "createdOn" : ISODate("2016-05-05T04:00:00.000Z"),
+    "__v" : 0
+}
